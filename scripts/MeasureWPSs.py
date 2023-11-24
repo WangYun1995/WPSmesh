521e3026b124433b77317f06787a3f6b54535e05
Yun Wang <68187127+WangYun1995@users.noreply.github.com>
2023-11-24 15:22:44 +0800

    Update MeasureWPSs.py

--- scripts/MeasureWPSs.py
+++ scripts/MeasureWPSs.py
@@ -48,7 +48,7 @@ print_status(comm,start_time,'Starting the calculations')
 # Measure the WPSs from the mesh
 Nscales = 25
 Ndens   = 8
-k_pseu, f_vol, env_WPS, global_WPS = WPSs( mesh1, mesh2, Nscales, Ndens, kmax=0.4, comm=comm)
+k_pseu, f_vol, env_WPS, global_WPS = WPSs( mesh1, mesh2, Nscales, densbins, kmax=0.4, wavelet=iso_cwgdw, comm=comm)
 
 #--------------------------------------------------------
 print_status(comm,start_time,'Done')
