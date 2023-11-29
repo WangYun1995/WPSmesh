3dccb5a7882ffd2d82da6abf91d78a7dabdbf7e0
Yun Wang <68187127+WangYun1995@users.noreply.github.com>
2023-11-29 15:43:28 +0800

    Update MeasureWPSs_subbox.py

--- scripts/MeasureWPSs_subbox.py
+++ scripts/MeasureWPSs_subbox.py
@@ -46,9 +46,11 @@ print_status(comm,start_time,'Starting the calculations')
 #--------------------------------------------------------
 
 # Measure the WPSs from the mesh
-Nscales = 25
-Ndens   = 8
-k_pseu, f_vol_sub, env_WPS_sub, global_WPS_sub = WPSs_subbox( mesh1, mesh2, Nscales, Ndens, kmax=0.4, Nsub=2, wavelet=iso_cwgdw, comm=None)
+Nscales   = 25
+Ndens     = 8
+bins_temp = np.geomspace(1e-1,1e+2,Ndens-1,endpoint=True) 
+densbins  = np.pad( bins_temp, (1, 1), 'constant', constant_values=(0,1e+100) )
+k_pseu, f_vol_sub, env_WPS_sub, global_WPS_sub = WPSs_subbox( mesh1, mesh2, Nscales, densbins, kmax=0.4, Nsub=2, wavelet=iso_cwgdw, comm=None)
 
 #--------------------------------------------------------
 print_status(comm,start_time,'Done')
