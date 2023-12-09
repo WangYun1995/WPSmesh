7b14edad033738421ca638f59ca03e78a353a3d3
Yun Wang <68187127+WangYun1995@users.noreply.github.com>
2023-12-09 11:27:38 +0800

    Update MeasureWPSs.py

--- scripts/MeasureWPSs.py
+++ scripts/MeasureWPSs.py
@@ -50,7 +50,7 @@ Nscales   = 25
 Ndens     = 8
 bins_temp = np.geomspace(1e-1,1e+2,Ndens-1,endpoint=True) 
 densbins  = np.pad( bins_temp, (1, 1), 'constant', constant_values=(0,1e+100) )
-k_pseu, f_vol, env_WPS, global_WPS = WPSs( mesh1, mesh2, Nscales, densbins, kmax=0.4, wavelet=iso_cwgdw, comm=comm)
+k_pseu, f_vol, env_WPS, global_WPS = WPSs( mesh1, mesh2, Nscales, densbins, kmax=0.4, wavelet='iso_cwgdw', comm=comm)
 
 #--------------------------------------------------------
 print_status(comm,start_time,'Done')
